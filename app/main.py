450c1d8f522d8665ec4cb57b2aa45fc2cdbf9cb8
YosriMlik <ymlik24@gmail.com>
2026-05-17 20:51:04 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         if command == "exit": break
-        if command.startswith("echo "): print(command[5:])
-        if command.startswith("type "):
+        elif command.startswith("echo "): print(command[5:])
+        elif command.startswith("type "):
             if(command[5:] in builtin_commands): print(f"{command[5:]} is a shell builtin")
             else: print(f"{command[5:]}: not found")
         else: print(f"{command}: command not found")
